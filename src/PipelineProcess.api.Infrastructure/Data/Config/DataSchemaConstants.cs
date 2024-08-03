50aa44265f5709593f770af3f8eb436653d3dbc6
Shahin Zamani <sh.zamani2002@gmail.com>
2024-08-03 22:31:17 +0330

    feat: add constants for configurations

--- src/PipelineProcess.api.Infrastructure/Data/Config/DataSchemaConstants.cs
+++ src/PipelineProcess.api.Infrastructure/Data/Config/DataSchemaConstants.cs
@@ -3,4 +3,7 @@
 public static class DataSchemaConstants
 {
   public const int DEFAULT_NAME_LENGTH = 100;
+  public const int DEFAULT_ID_LENGTH = 64;
+  public const int DEFAULT_TITLE_LENGTH = 256;
+  public const int DEFAULT_DESCRIPTION_LENGTH = 512;
 }

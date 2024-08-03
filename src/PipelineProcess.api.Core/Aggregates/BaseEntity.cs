f0c3da1b74e6979dc5eddcefa0d31ca8481bf3e5
Shahin Zamani <sh.zamani2002@gmail.com>
2024-08-03 22:30:21 +0330

    feat: add Custom BaseEntity

---
+++ src/PipelineProcess.api.Core/Aggregates/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.SharedKernel;
+
+namespace PipelineProcess.api.Core.Aggregates;
+
+public abstract class BaseEntity<TId>: EntityBase<TId>
+  where TId : struct, IEquatable<TId>
+{
+  public DateTime CreatedAt { get; protected set; }
+  public bool IsRemoved { get; protected set; }
+  public DateTime? ModifiedAt { get; protected set; }
+}

b40f30080c6bccd34a89bc4ab4eea478a50b7b94
Shahin Zamani <sh.zamani2002@gmail.com>
2024-08-05 21:43:21 +0330

    feat: GetProjectById Service

---
+++ src/PipelineProcess.api.Core/Aggregates/ProjectAggregate/Specifications/GetAllProjectsSpec.cs
@@ -0,0 +1,17 @@
+﻿using Ardalis.Specification;
+using PipelineProcess.api.Core.Records;
+
+namespace PipelineProcess.api.Core.Aggregates.ProjectAggregate.Specifications;
+
+public class GetAllProjectsSpec : Specification<Project, GetProjectByIdRecord>
+{
+  public GetAllProjectsSpec(Guid projectId, bool AsReadOnly = true)
+  {
+    Query
+      .Where(x => x.Id == projectId)
+      .AsNoTracking(AsReadOnly);
+
+    Query
+      .Select(x => new GetProjectByIdRecord(x.Description, x.Id));
+  }
+}
---
+++ src/PipelineProcess.api.UseCases/Services/Projects/Queries/GetProjectByIRequestQuery.cs
@@ -0,0 +1,23 @@
+﻿using Ardalis.Result;
+using Ardalis.SharedKernel;
+using PipelineProcess.api.Core.Aggregates.ProjectAggregate;
+using PipelineProcess.api.Core.Aggregates.ProjectAggregate.Specifications;
+using PipelineProcess.api.Core.Records;
+
+namespace PipelineProcess.api.UseCases.Services.Projects.Queries;
+
+public class GetProjectByIdQuery(Guid projectId) : IQuery<Result<GetProjectByIdRecord>>
+{
+  public Guid ProjectId { get; set; } = projectId;
+}
+
+public class GetProjectByIdQueryHandler(IReadRepository<Project> repository)
+  : IQueryHandler<GetProjectByIdQuery, Result<GetProjectByIdRecord>>
+{
+  private readonly IReadRepository<Project> _repository = repository;
+  public async Task<Result<GetProjectByIdRecord>> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
+  {
+    var projects = await _repository.FirstOrDefaultAsync(new GetAllProjectsSpec(request.ProjectId), cancellationToken);
+    return projects is null ? Result<GetProjectByIdRecord>.NotFound() : Result<GetProjectByIdRecord>.Success(projects);
+  }
+}

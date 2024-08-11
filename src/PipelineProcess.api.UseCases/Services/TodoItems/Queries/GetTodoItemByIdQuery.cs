311ee6b17999311d8038f2e9ac5d686420c2ebb3
Shahin Zamani <sh.zamani2002@gmail.com>
2024-08-11 12:21:25 +0330

    feat: GetTodoItem by id Entity

---
+++ src/PipelineProcess.api.UseCases/Services/TodoItems/Queries/GetTodoItemByIdQuery.cs
@@ -0,0 +1,28 @@
+﻿using Ardalis.Result;
+using Ardalis.SharedKernel;
+using PipelineProcess.api.Core.Aggregates.TodoItemAggregate;
+using PipelineProcess.api.Core.Aggregates.TodoItemAggregate.Specifications;
+using PipelineProcess.api.Core.Records;
+
+namespace PipelineProcess.api.UseCases.Services.TodoItems.Queries;
+
+public class GetTodoItemByIdQuery(string id) : IQuery<Result<GetTodoItemDto>>
+{
+  public string Id { get; set; } = id;
+}
+
+public class GetTodoItemByIdQueryHandler(IReadRepository<TodoItem> repository)
+  : IQueryHandler<GetTodoItemByIdQuery, Result<GetTodoItemDto>>
+{
+  private readonly IReadRepository<TodoItem> _repository = repository;
+  public async Task<Result<GetTodoItemDto>> Handle(GetTodoItemByIdQuery request, CancellationToken cancellationToken)
+  {
+    var todoItem = await _repository.FirstOrDefaultAsync(new GetTodoItemByIdSpec(request.Id), cancellationToken);
+
+    if (todoItem is null)
+      return Result<GetTodoItemDto>.NotFound();
+    
+    return Result<GetTodoItemDto>.Success(todoItem);
+    
+  }
+}

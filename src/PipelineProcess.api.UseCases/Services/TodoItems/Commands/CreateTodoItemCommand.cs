ccf58f1902ba2f1c22f6ff32d9dfa8a874ee65b2
Shahin Zamani <sh.zamani2002@gmail.com>
2024-08-11 12:20:20 +0330

    feat: create todoitem service

---
+++ src/PipelineProcess.api.UseCases/Services/TodoItems/Commands/CreateTodoItemCommand.cs
@@ -0,0 +1,27 @@
+﻿using Ardalis.Result;
+using Ardalis.SharedKernel;
+using PipelineProcess.api.Core.Aggregates.TodoItemAggregate;
+
+namespace PipelineProcess.api.UseCases.Services.TodoItems.Commands;
+
+public class CreateTodoItemCommand(string title, string description) : ICommand<Result<string>>
+{
+  public string Title { get; set; } = title;
+  public string Description { get; set; } = description;
+  
+}
+
+public class CreateTodoItemCommandHandler(IRepository<TodoItem> repository)
+  : ICommandHandler<CreateTodoItemCommand, Result<string>>
+{
+  private readonly IRepository<TodoItem> _repository = repository;
+  public async Task<Result<string>> Handle(CreateTodoItemCommand request, CancellationToken cancellationToken)
+  {
+    var todoItem = new TodoItem(request.Title, request.Description);
+
+    await _repository.AddAsync(todoItem, cancellationToken);
+    await _repository.SaveChangesAsync(cancellationToken);
+
+    return Result<string>.Success(todoItem.Id.ToString());
+  }
+}
---
+++ src/PipelineProcess.api.Web/Endpoints/TodoItems/CreateTodoItem.cs
@@ -0,0 +1,39 @@
+﻿using PipelineProcess.api.UseCases.Services.TodoItems.Commands;
+
+namespace PipelineProcess.api.Web.Endpoints.TodoItems;
+
+public class CreateTodoItem : Endpoint<CreateTodoItem.CreateTodoItemRequest, Result<string>>
+{
+  private readonly IMediator _mediator;
+
+  public CreateTodoItem(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
+
+  public override void Configure()
+  {
+    Post(CreateTodoItemRequest.Route);
+    AllowAnonymous();
+    Summary(s =>
+    {
+      s.Summary = "Create a new TodoItem.";
+      s.Description = "Create a new TodoItem. A valid Title is required.";
+    });
+    Tags([nameof(CreateTodoItemRequest)]);
+  }
+
+  public override async Task<Result<string>> ExecuteAsync(CreateTodoItemRequest req, CancellationToken ct)
+  {
+    var result = await _mediator.Send(new CreateTodoItemCommand(req.Title ?? string.Empty, req.Description ?? string.Empty), ct);
+    if (result.IsSuccess)
+      Response = result.Value;
+    return result;
+  }
+  public class CreateTodoItemRequest
+  {
+    public const string Route = "/api/TodoItem/Create";
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+  }
+}

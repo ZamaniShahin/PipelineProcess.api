282b9af635527487140f89d8dfb1372f921566f6
Shahin Zamani <sh.zamani2002@gmail.com>
2024-08-11 12:22:01 +0330

    feat: Update TodoItem service

---
+++ src/PipelineProcess.api.UseCases/Services/TodoItems/Commands/UpdateTodoItemCommand.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.Result;
+using Ardalis.SharedKernel;
+using PipelineProcess.api.Core.Aggregates.TodoItemAggregate;
+
+namespace PipelineProcess.api.UseCases.Services.TodoItems.Commands;
+
+public class UpdateTodoItemCommand(string id, string title, string description)
+  : Ardalis.SharedKernel.ICommand<Result<string>>
+{
+  public string Title { get; set; } = !string.IsNullOrEmpty(title) ? title : string.Empty;
+  public string Description { get; set; } = description;
+  public string Id { get; set; } = id;
+}
+
+public class UpdateTodoItemCommandHandler(IRepository<TodoItem> repository)
+  : ICommandHandler<UpdateTodoItemCommand, Result<string>>
+{
+  private readonly IRepository<TodoItem> _repository = repository;
+
+  public async Task<Result<string>> Handle(UpdateTodoItemCommand request, CancellationToken cancellationToken)
+  {
+    var todoItem = await _repository.GetByIdAsync(request.Id, cancellationToken);
+
+    if (todoItem is null)
+      return Result<string>.NotFound();
+
+    todoItem.Update(request.Title, request.Description);
+
+    await _repository.UpdateAsync(todoItem, cancellationToken);
+    await _repository.SaveChangesAsync(cancellationToken);
+
+    return Result<string>.Success(todoItem.Id.ToString());
+  }
+}
---
+++ src/PipelineProcess.api.Web/Endpoints/TodoItems/UpdateTodoItem.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Build.Framework;
+using PipelineProcess.api.UseCases.Services.TodoItems.Commands;
+
+namespace PipelineProcess.api.Web.Endpoints.TodoItems;
+
+public class UpdateTodoItem : Endpoint<UpdateTodoItem.UpdateTodoItemRequest, Result<string>>
+{
+  private readonly IMediator _mediator;
+
+  public UpdateTodoItem(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
+  public class UpdateTodoItemRequest
+  {
+    public const string Route = "/api/Schema/{TodoItemId}/Update";
+    public static string BuildRoute(string todoItemId) => Route.Replace("{TodoItemId}", todoItemId.ToString());
+    public string? TodoItemId { get; set; }
+    [Required]
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+  }
+  public override void Configure()
+  {
+    Put(UpdateTodoItemRequest.Route);
+    AllowAnonymous();
+    Summary(s =>
+    {
+      s.Summary = "Update a TodoItem.";
+      s.Description = "Update a TodoItem. A valid Title is required.";
+    });
+    Tags([nameof(Schemas)]);
+  }
+
+  public override async Task<Result<string>> ExecuteAsync(UpdateTodoItemRequest req, CancellationToken ct)
+  {
+    var result = await _mediator.Send(new UpdateTodoItemCommand(
+      req.TodoItemId ?? string.Empty,
+      req.Title ?? string.Empty,
+      req.Description ?? string.Empty), ct);
+    
+    
+    if (result.IsSuccess)
+      Response = result.Value;
+    
+    return result;
+  }
+}

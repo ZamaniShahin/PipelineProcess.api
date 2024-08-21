51d9ec2d318b51cc4884b37c95a07e65dfffe74e
Shahin Zamani <sh.zamani2002@gmail.com>
2024-08-21 10:15:30 +0330

    cheat: update Entities in database

---
+++ src/PipelineProcess.api.Infrastructure/Migrations/20240821064317_RenameEntities.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PipelineProcess.api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectTodoItemEntity");
+
+            migrationBuilder.DropTable(
+                name: "TodoItem");
+
+            migrationBuilder.CreateTable(
+                name: "Process",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", maxLength: 64, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Process", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProcessSheetEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    AdminAcceptStatus = table.Column<int>(type: "int", nullable: false),
+                    TodoItemId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ProjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessSheetEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProcessSheetEntity_Process_TodoItemId",
+                        column: x => x.TodoItemId,
+                        principalTable: "Process",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ProcessSheetEntity_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcessSheetEntity_ProjectId",
+                table: "ProcessSheetEntity",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcessSheetEntity_TodoItemId",
+                table: "ProcessSheetEntity",
+                column: "TodoItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcessSheetEntity");
+
+            migrationBuilder.DropTable(
+                name: "Process");
+
+            migrationBuilder.CreateTable(
+                name: "TodoItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", maxLength: 64, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Title = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TodoItem", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectTodoItemEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    TodoItemId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    AdminAcceptStatus = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsRemoved = table.Column<bool>(type: "bit", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectTodoItemEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectTodoItemEntity_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ProjectTodoItemEntity_TodoItem_TodoItemId",
+                        column: x => x.TodoItemId,
+                        principalTable: "TodoItem",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTodoItemEntity_ProjectId",
+                table: "ProjectTodoItemEntity",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTodoItemEntity_TodoItemId",
+                table: "ProjectTodoItemEntity",
+                column: "TodoItemId");
+        }
+    }
+}

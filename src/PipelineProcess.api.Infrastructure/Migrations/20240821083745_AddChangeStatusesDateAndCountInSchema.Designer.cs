60a60c2a70ce2c15b2f4ee0f89120f58ac34f0bc
Shahin Zamani <sh.zamani2002@gmail.com>
2024-08-21 12:09:12 +0330

    feat: add migration for add datetimes and Count in Schema

--- src/PipelineProcess.api.Infrastructure/Migrations/20240821083745_AddChangeStatusesDateAndCountInSchema.Designer.cs
+++ src/PipelineProcess.api.Infrastructure/Migrations/20240821083745_AddChangeStatusesDateAndCountInSchema.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PipelineProcess.api.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using PipelineProcess.api.Infrastructure.Data;
 namespace PipelineProcess.api.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240821083745_AddChangeStatusesDateAndCountInSchema")]
+    partial class AddChangeStatusesDateAndCountInSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/PipelineProcess.api.Infrastructure/Migrations/20240821083745_AddChangeStatusesDateAndCountInSchema.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PipelineProcess.api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddChangeStatusesDateAndCountInSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Count",
+                table: "Schema",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AdminAcceptStatusChangedAt",
+                table: "ProcessSheetEntity",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StatusChangedAt",
+                table: "ProcessSheetEntity",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "Schema");
+
+            migrationBuilder.DropColumn(
+                name: "AdminAcceptStatusChangedAt",
+                table: "ProcessSheetEntity");
+
+            migrationBuilder.DropColumn(
+                name: "StatusChangedAt",
+                table: "ProcessSheetEntity");
+        }
+    }
+}

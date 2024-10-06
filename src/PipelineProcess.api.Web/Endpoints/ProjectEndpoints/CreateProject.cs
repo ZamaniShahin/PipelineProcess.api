9fadb6eea95ed19b80d3338333cfbef1394bd7cc
Shahin Zamani <sh.zamani2002@gmail.com>
2024-10-06 11:53:06 +0330

    feat: init

--- src/PipelineProcess.api.Web/Endpoints/ProjectEndpoints/CreateProject.cs
+++ src/PipelineProcess.api.Web/Endpoints/ProjectEndpoints/CreateProject.cs
@@ -19,7 +19,14 @@ public class CreateProject: Endpoint<CreateProject.CreateProjectRequest, Result<
     {
       s.Summary = "Create a new Project.";
       s.Description = "Create a new Project.";
-      s.ExampleRequest = new CreateProjectRequest { Description = "Project Description"};
+      s.ExampleRequest = new CreateProjectRequest
+      {
+        Description = "Project Description",
+        SchemaId = Guid.Parse("1B8E5401-E7A5-486C-AB97-48868C1EF0CF")
+        
+      };
+      s.ResponseExamples.Add(200, Result<string>.Success("Ha HA Ha"));
+      s.ResponseExamples.Add(404, Result<string>.NotFound("Nothing-Found"));
     });
     Tags(nameof(ProjectEndpoints));
   }
@@ -34,8 +41,7 @@ public class CreateProject: Endpoint<CreateProject.CreateProjectRequest, Result<
   {
     public const string Route = "/api/Project/{SchemaId}/Create";
     public static string BuildRoute(string schemaId) => Route.Replace("{SchemaId}", schemaId.ToString());
-    public Guid SchemaId { get; set; }
-    [Required]
+    [BindFrom("SchemaId")]public Guid SchemaId { get; set; }
     public string? Description { get; set; }
   }
 }
